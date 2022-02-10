056f4350f5a6752e862aa5a30af4ecaa6f5a770e
qtrex <49007204+qtrex@users.noreply.github.com>
2022-02-09 18:54:48 -0800

    🗎 IT-36 Manual Merge of Harjap's Branch

---
+++ frontend/src/Navigation.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Button, Navbar, Nav, Container, NavDropdown, Form, FormControl } from 'react-bootstrap';
+
+function Navigation() {
+    return (
+        <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Asian N Cajun</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">About</Nav.Link>
+        <Nav.Link href="#action2">Order</Nav.Link>
+        <NavDropdown title="Reservations" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+
+      </Nav>
+      <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search"
+          className="me-2"
+          aria-label="Search"
+        />
+        <Button variant="outline-success">Search</Button>
+      </Form>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+    )
+}
+
+export default Navigation
