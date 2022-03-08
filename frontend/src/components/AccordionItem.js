93de433a8a7f37ce40d2c94084dd7559c38548f8
Alder Moreno <50600863+aldermo98@users.noreply.github.com>
2022-03-08 04:07:44 -0800

    made the accordion menu dynamic

--- frontend/src/components/AccordionItem.js
+++ frontend/src/components/AccordionItem.js
@@ -1,20 +1,21 @@
 import React, { useState } from 'react'
-const SingleQuestion = ({ title, info }) => {
+
+const Item = ({ title, content }) => {
   const [expanded, setExpanded] = useState(false)
 
   return (
-    <article className='question'>
+    <article className='item'>
       <header onClick={() => setExpanded(!expanded)}>
-        <h6 className='question-title'>
+        <h6 className='item-title'>
           {title}
         </h6>
         <button className='btn'>
           {expanded ? <i class="fa-solid fa-minus"></i> : <i class="fa-solid fa-plus"></i>}
         </button>
       </header>
-      {expanded && <p>{info}</p>}
+      {expanded && <div className='content'>{content}</div>}
     </article>
   )
 }
 
-export default SingleQuestion
+export default Item
--- frontend/src/components/CareersButton.js
+++ frontend/src/components/CareersButton.js
@@ -1,40 +1,55 @@
 import Modal from 'react-bootstrap/Modal'
-import {Button} from 'react-bootstrap';
+import {Form, ToggleButtonGroup, ToggleButton, Button} from 'react-bootstrap';
 import { useState, ReactDOM} from 'react';
 import AccordionMenu from './AccordionMenu';
-import CareersForm from './CareersForm';
+import CareerInfo from './CareerInfo.js'
+import { FormLabel } from 'react-bootstrap';
+import { FormControl } from 'react-bootstrap';
 
 function CareersButton() {
     const [show, setShow] = useState(false);
+    const [value, setValue] = useState();
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleChange = (val) => setValue(val);
 
     return (
       <>
         <a href='#' onClick={handleShow}>
           Careers
         </a>
-
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>Career Sign Up</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            <AccordionMenu/>
-            <CareersForm />
-          </Modal.Body>
-
-          <Modal.Footer>
-            <Button variant="secondary" onClick={handleClose}>
-              Cancel
-            </Button>
-            <Button variant="primary" onClick={handleClose}>
-              Submit
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <Form>
+          <Modal show={show} onHide={handleClose} className="orderModal">
+            <Modal.Header closeButton>
+              <Modal.Title>Career Sign Up</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <AccordionMenu data={CareerInfo}/>
+              <hr></hr>
+                  <div>
+                      <FormLabel>What positions are your interested in?</FormLabel>
+                      <ToggleButtonGroup type="checkbox" value={value} onChange={handleChange}>
+                        <ToggleButton id="tbg-btn-1" variant="outline-dark" size="sm" className='mx-1' value={1}>Pos1</ToggleButton>
+                        <ToggleButton id="tbg-btn-2" variant="outline-dark" size="sm" className='mx-1' value={2}>Pos2</ToggleButton>
+                        <ToggleButton id="tbg-btn-3" variant="outline-dark" size="sm" className='mx-1' value={3}>Pos3</ToggleButton>
+                        <ToggleButton id="tbg-btn-4" variant="outline-dark" size="sm" className='mx-1' value={4}>Pos4</ToggleButton>
+                        <ToggleButton id="tbg-btn-5" variant="outline-dark" size="sm" className='mx-1' value={5}>Pos5</ToggleButton>
+                      </ToggleButtonGroup>
+                      <FormControl type="hidden"></FormControl>
+                  </div>
+                  <div>
+                      <FormLabel for="resume">Resume: </FormLabel>
+                      <FormControl type="file" class="form-control" id="resumeAttachment"></FormControl>
+                  </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>Cancel</Button>
+              <Button type="submit" variant="primary" onClick={handleClose}>Submit</Button>
+            </Modal.Footer>
+          </Modal>
+        </Form>
+        
       </>
     );
   }
