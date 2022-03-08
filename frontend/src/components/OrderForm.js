9e5a6f232a82fa761758909629ef54032182b787
Alder Moreno <50600863+aldermo98@users.noreply.github.com>
2022-03-08 00:22:27 -0800

    refactors and more
    refactor, css cleanup,  nav collapse fix and active link implementation, reservation button implementation

--- frontend/src/components/OrderForm.js
+++ frontend/src/components/OrderForm.js
@@ -2,18 +2,23 @@ import Modal from 'react-bootstrap/Modal'
 import {Button} from 'react-bootstrap';
 import { useState, ReactDOM} from 'react';
 import { Link } from 'react-router-dom';
+import grubhub from '../img/grubhub.svg';
+import ubereats from '../img/ubereats.svg';
+import postmates from '../img/postmates.svg';
+import yelp from '../img/yelp.svg';
 
 function OrderForm(){
     return(
         <form>
-            <ul class="list-unstyled mb-0 service-select">
-                <li class="mb-4"><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src="https://cdn.cdnlogo.com/logos/g/61/grubhub.svg" className=" w-75"/></a></li>
-                <li class="mb-0"><a href="#" target="_blank"><img src="https://cdn.cdnlogo.com/logos/u/49/uber-eats.svg" className=" w-75"/></a></li>
-                <li class="mb-0"><a href="#" target="_blank"><img src="https://www.vectorlogo.zone/logos/postmates/postmates-ar21.svg" className=" w-75"/></a></li>
+            <ul class="list-unstyled mb-0 services">
+                <li class="mb-4 text-center"><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src={grubhub} className="icons w-75"/></a></li>
+                <li class="mb-4 text-center"><a href="#" target="_blank"><img src={ubereats} className="icons w-75"/></a></li>
+                <li class="mb-2 text-center"><a href="#" target="_blank"><img src={postmates} className="icons w-75"/></a></li>
+                <li class="mb-2 text-center"><a href="https://www.yelp.com/biz/asian-n-cajun-2-fair-oaks-2" target="_blank"><img src={yelp} className="icons w-50"/></a></li>
             </ul>
             <hr></hr>
             <div>
-                <p>You can also check out our menu <Link to="/menu" class="obviousLink">here</Link> and place a pickup order through the phone!</p>
+                <p class="text-center">You can also check out our menu <Link to="/menu" class="hypertext">here</Link> and place a pickup order through the phone!</p>
             </div>
         </form>
     );
