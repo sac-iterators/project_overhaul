052cd1a05cb410a677b2c0cf08885c58602eeebe
Patrick <pchanthilack@gmail.com>
2022-03-09 15:09:32 -0800

    IT-91
    Fixed Ubereats button to link with Asian N Cajun 2 business on Ubereats for take out.

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -21,7 +21,8 @@ function Footer() {
             </ul>
             <ul class="list-inline services">
               <li class=""><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src="https://cdn.cdnlogo.com/logos/g/61/grubhub.svg" class="icons"/></a></li>
-              <li class=""><a href="#" target="_blank"><img src="https://img.icons8.com/color/48/000000/uber-eats-app.png" class="icons"/></a></li>
+              <li class=""><a href="https://www.ubereats.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkFzaWFuJTIwTiclMjBDYWp1biUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmM4YzlhY2FkLTRjZjEtMTg1ZS1iMDE5LWVmMmFlZmFiYWIxNSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMzguNjQ5MDYlMkMlMjJsb25naXR1ZGUlMjIlM0EtMTIxLjUxMDA5NjclN0Q%3D" 
+                target="_blank"><img src="https://img.icons8.com/color/48/000000/uber-eats-app.png" class="icons"/></a></li>
               <li class=""><a href="#" target="_blank"><img src="https://iconape.com/wp-content/files/rd/245476/png/postmates-logo.png" class="icons"/></a></li>
             </ul>
           </div>
--- frontend/src/components/OrderForm.js
+++ frontend/src/components/OrderForm.js
@@ -8,7 +8,7 @@ function OrderForm(){
         <form>
             <ul class="list-unstyled mb-0 service-select">
                 <li class="mb-4"><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src="https://cdn.cdnlogo.com/logos/g/61/grubhub.svg" className=" w-75"/></a></li>
-                <li class="mb-0"><a href="#" target="_blank"><img src="https://cdn.cdnlogo.com/logos/u/49/uber-eats.svg" className=" w-75"/></a></li>
+                <li class="mb-0"><a href="https://www.ubereats.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkFzaWFuJTIwTiclMjBDYWp1biUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmM4YzlhY2FkLTRjZjEtMTg1ZS1iMDE5LWVmMmFlZmFiYWIxNSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMzguNjQ5MDYlMkMlMjJsb25naXR1ZGUlMjIlM0EtMTIxLjUxMDA5NjclN0Q%3D" target="_blank"><img src="https://cdn.cdnlogo.com/logos/u/49/uber-eats.svg" className=" w-75"/></a></li>
                 <li class="mb-0"><a href="#" target="_blank"><img src="https://www.vectorlogo.zone/logos/postmates/postmates-ar21.svg" className=" w-75"/></a></li>
             </ul>
             <hr></hr>
