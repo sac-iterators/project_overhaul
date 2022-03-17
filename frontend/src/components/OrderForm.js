b7c0fdd366d43f53d6f5e343503f8dff49d14a54
Alder Moreno <50600863+aldermo98@users.noreply.github.com>
2022-03-16 18:51:44 -0700

    modified careers popup and removed class react syntax errors

--- frontend/src/components/OrderForm.js
+++ frontend/src/components/OrderForm.js
@@ -10,15 +10,15 @@ import yelp from '../img/yelp.svg';
 function OrderForm(){
     return(
         <form>
-            <ul class="list-unstyled mb-0 service-select">
-                <li class="mb-4"><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src="https://cdn.cdnlogo.com/logos/g/61/grubhub.svg" className=" w-75"/></a></li>
-                <li class="mb-0"><a href="https://www.ubereats.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkFzaWFuJTIwTiclMjBDYWp1biUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmM4YzlhY2FkLTRjZjEtMTg1ZS1iMDE5LWVmMmFlZmFiYWIxNSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMzguNjQ5MDYlMkMlMjJsb25naXR1ZGUlMjIlM0EtMTIxLjUxMDA5NjclN0Q%3D" target="_blank"><img src="https://cdn.cdnlogo.com/logos/u/49/uber-eats.svg" className=" w-75"/></a></li>
-                <li class="mb-0"><a href="https://postmates.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg" target="_blank"><img src="https://www.vectorlogo.zone/logos/postmates/postmates-ar21.svg" className=" w-75"/></a></li>
+            <ul className="list-unstyled mb-0 service-select">
+                <li className="mb-4"><a href="https://www.grubhub.com/restaurant/new-asian-n-cajun-2-greenback-ln-8121-greenback-ln-fair-oaks/3048809" target="_blank"><img src="https://cdn.cdnlogo.com/logos/g/61/grubhub.svg" className=" w-75"/></a></li>
+                <li className="mb-0"><a href="https://www.ubereats.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg?diningMode=DELIVERY&pl=JTdCJTIyYWRkcmVzcyUyMiUzQSUyMkFzaWFuJTIwTiclMjBDYWp1biUyMiUyQyUyMnJlZmVyZW5jZSUyMiUzQSUyMmM4YzlhY2FkLTRjZjEtMTg1ZS1iMDE5LWVmMmFlZmFiYWIxNSUyMiUyQyUyMnJlZmVyZW5jZVR5cGUlMjIlM0ElMjJ1YmVyX3BsYWNlcyUyMiUyQyUyMmxhdGl0dWRlJTIyJTNBMzguNjQ5MDYlMkMlMjJsb25naXR1ZGUlMjIlM0EtMTIxLjUxMDA5NjclN0Q%3D" target="_blank"><img src="https://cdn.cdnlogo.com/logos/u/49/uber-eats.svg" className=" w-75"/></a></li>
+                <li className="mb-0"><a href="https://postmates.com/store/asian-n-cajun-2/m7JzYDLnRESi966_6EHoSg" target="_blank"><img src="https://www.vectorlogo.zone/logos/postmates/postmates-ar21.svg" className=" w-75"/></a></li>
 
             </ul>
             <hr></hr>
             <div>
-                <p class="text-center">You can also check out our menu <Link to="/menu" class="hypertext">here</Link> and place a pickup order through the phone!</p>
+                <p className="text-center">You can also check out our menu <Link to="/menu" className="hypertext">here</Link> and place a pickup order through the phone!</p>
             </div>
         </form>
     );
